3c9ccbe1b3cbc347fdca55687ea4a19a65dc7ecd
indre101 <zygaityte.indre@gmail.com>
2020-05-12 16:02:45 +0200

    Updates

---
+++ precache-manifest.6942061ec6b4eeab4cf248661a99f254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a346435028536e60ae4a66c35f6e387c",
+    "url": "/more-beer-less-tears-dashboard/index.html"
+  },
+  {
+    "revision": "a532da36f01adb3c63db",
+    "url": "/more-beer-less-tears-dashboard/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9498e20afb99bbe0d729",
+    "url": "/more-beer-less-tears-dashboard/static/js/2.e2e94ac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/more-beer-less-tears-dashboard/static/js/2.e2e94ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a532da36f01adb3c63db",
+    "url": "/more-beer-less-tears-dashboard/static/js/main.0f253f34.chunk.js"
+  },
+  {
+    "revision": "cd579f6ae5a42e358f28",
+    "url": "/more-beer-less-tears-dashboard/static/js/runtime-main.996c3d17.js"
+  }
+]);
